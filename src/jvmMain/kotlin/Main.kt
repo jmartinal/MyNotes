f073729eebed721db64ee977aa8c625d941d51bf
Jorge Martin <jorge.martin@pulselive.com>
2023-06-01 12:38:40 +0200

    Exercise: 2.1 Variables, types and string templates

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,26 +1,40 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+    val text = remember { mutableStateOf("") }
+    val buttonEnabled = text.value.isNotEmpty()
+    val greetings = if (text.value.isEmpty()) "Who's there?" else "Hello ${text.value}"
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column {
+            TextField(
+                value = text.value,
+                onValueChange = { newText ->
+                    text.value = newText
+                }
+            )
+            Text(
+                text = greetings
+            )
+            Button(
+                onClick = { text.value = ""},
+                enabled = buttonEnabled
+            ) {
+                Text("Clean")
+            }
         }
     }
 }
