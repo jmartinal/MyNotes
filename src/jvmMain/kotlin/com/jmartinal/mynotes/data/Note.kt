14d1c8e04d20ff075e5e66344d302e1b9c33677b
Jorge Martin <jorge.martin@pulselive.com>
2023-06-30 11:44:07 +0200

    Exercise: 6.8 Consume MyNotesServer's notes endpoint

--- src/jvmMain/kotlin/com/jmartinal/mynotes/data/Note.kt
+++ src/jvmMain/kotlin/com/jmartinal/mynotes/data/Note.kt
@@ -1,31 +1,16 @@
 package com.jmartinal.mynotes.data
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class Note(
     val title: String,
     val description: String,
     val type: Type
 ) {
-
     enum class Type {
         TEXT,
         AUDIO
     }
 }
-
-fun getNotes(size: Int): Flow<List<Note>> = flow {
-    delay(2000)
-    val notes = (1..size).map { position ->
-        Note(
-            title = "Title $position",
-            description = "Description $position",
-            type = if (position % 3 == 0) Note.Type.AUDIO else Note.Type.TEXT
-        )
-    }
-    emit(notes)
-}
-
 
--- src/jvmMain/kotlin/com/jmartinal/mynotes/ui/screens/home/HomeState.kt
+++ src/jvmMain/kotlin/com/jmartinal/mynotes/ui/screens/home/HomeState.kt
@@ -2,11 +2,12 @@ package com.jmartinal.mynotes.ui.screens.home
 
 import com.jmartinal.mynotes.data.Filter
 import com.jmartinal.mynotes.data.Note
-import com.jmartinal.mynotes.data.getNotes
+import com.jmartinal.mynotes.data.remote.notesClient
+import io.ktor.client.call.*
+import io.ktor.client.request.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
@@ -14,11 +15,11 @@ object HomeState {
     private val _state = MutableStateFlow(UiState())
     val state = _state.asStateFlow()
 
-    fun loadNotes(coroutineScope: CoroutineScope, size: Int = 10) = coroutineScope.launch {
+    fun loadNotes(coroutineScope: CoroutineScope) = coroutineScope.launch {
         _state.value = UiState(loading = true)
-        getNotes(size).collect {
-            _state.value = UiState(loading = false, notes = it)
-        }
+        val response = notesClient.request("http://localhost:8080/notes")
+        _state.value = UiState(loading = false, notes = response.body())
+        println(response.body() as String)
     }
 
     fun onFilterClicked(filter: Filter) {
