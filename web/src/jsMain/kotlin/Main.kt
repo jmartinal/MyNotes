1d15ea7ea6394bcdf1ac51c61ad5f3c31a06b5bd
Jorge Martin <jorge.martin@pulselive.com>
2023-12-04 12:03:17 +0100

    Exercise: 8.4 Introduction to Compose Web

--- web/src/jsMain/kotlin/Main.kt
+++ web/src/jsMain/kotlin/Main.kt
@@ -1,33 +1,48 @@
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import com.jmartinal.mynotes.getPlatformName
-import org.jetbrains.compose.web.css.*
-import org.jetbrains.compose.web.dom.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import com.jmartinal.mynotes.data.Note
+import com.jmartinal.mynotes.ui.screens.home.HomeViewModel
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.H3
+import org.jetbrains.compose.web.dom.Span
+import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.renderComposable
 
 fun main() {
-    var count: Int by mutableStateOf(0)
 
     renderComposable(rootElementId = "root") {
-        Div({ style { padding(25.px) } }) {
-            H1 { Text(getPlatformName()) }
+        val coroutineScope = rememberCoroutineScope()
+        val homeViewModel = remember { HomeViewModel(coroutineScope) }
 
-            Button(attrs = {
-                onClick { count -= 1 }
-            }) {
-                Text("-")
-            }
+        NotesList(homeViewModel.state.filteredNotes.orEmpty()) { note ->
+            console.log("Note clicked: $note")
+        }
+    }
+}
 
-            Span({ style { padding(15.px) } }) {
-                Text("$count")
-            }
+@Composable
+private fun NotesList(notes: List<Note>, onNoteClick: (Note) -> Unit) {
+    Div {
+        notes.forEach { note ->
+            NoteCard(note, onNoteClick)
+        }
+    }
+}
 
-            Button(attrs = {
-                onClick { count += 1 }
-            }) {
-                Text("+")
-            }
+@Composable
+private fun NoteCard(note: Note, onNoteClick: (Note) -> Unit) {
+    Div(
+        attrs = {
+            onClick { onNoteClick(note) }
+        }
+    ) {
+        H3 { Text(note.title) }
+
+        if (note.type == Note.Type.AUDIO) Span { Text("🎙️") }
+
+        Div {
+            Text(note.description)
         }
     }
 }
