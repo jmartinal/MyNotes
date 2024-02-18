570114db7f5bb9ad3c300e47e0da33592a68bcec
Jorge Martin <jorge.martin@pulselive.com>
2024-02-18 12:20:27 +0100

    Exercise: 9.2 Creating the Android app

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,25 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+rootProject.name = "MyNotes"
+
 pluginManagement {
     repositories {
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
         gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
-rootProject.name = "MyNotes"
-
 include(
     ":common",
     ":desktop",
-    ":web"
+    ":web",
+    ":android"
 )
